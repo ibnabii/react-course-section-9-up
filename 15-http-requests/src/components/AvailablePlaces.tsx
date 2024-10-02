f1c87e9990a211f0ba36f0e1f852941b15c85e23
ibnabii <someone@somewhere.net>
2024-10-02 12:54:41 +0200

    First fetch

--- 15-http-requests/src/components/AvailablePlaces.tsx
+++ 15-http-requests/src/components/AvailablePlaces.tsx
@@ -1,5 +1,6 @@
 import Places from "./Places.jsx";
 import { type PlaceType } from "./TypesForOldComponents.tsx";
+import { useEffect, useState } from "react";
 
 type AvailablePlacesProps = {
   onSelectPlace: (selectedPlace: PlaceType) => void;
@@ -8,10 +9,20 @@ type AvailablePlacesProps = {
 export default function AvailablePlaces({
   onSelectPlace,
 }: AvailablePlacesProps) {
+  const [availablePlaces, setAvailablePlaces] = useState<PlaceType[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => response.json())
+      .then((data) => {
+        setAvailablePlaces(data.places);
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
