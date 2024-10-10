0b25efe98224276e49af790d569af6a22ccbb93b
ibnabii <someone@somewhere.net>
2024-10-10 20:51:30 +0200

    Section 17.253 - individual pieces of state

--- 17-forms/src/components/Login.tsx
+++ 17-forms/src/components/Login.tsx
@@ -1,6 +1,17 @@
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState<string>("");
+  const [enteredPassword, setEnteredPassword] = useState<string>("");
+
+  function handleEmailChange(event: ChangeEvent<HTMLInputElement>) {
+    setEnteredEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event: ChangeEvent<HTMLInputElement>) {
+    setEnteredPassword(event.target.value);
+  }
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
   }
@@ -11,12 +22,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleEmailChange}
+            value={enteredEmail}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={enteredPassword}
+            onChange={handlePasswordChange}
+          />
         </div>
       </div>
 
