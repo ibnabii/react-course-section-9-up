a28a103d36ec89a287a7935042f82cb1e9a45732
ibnabii <someone@somewhere.net>
2024-10-11 18:22:15 +0200

    Section 17.260 - validatino via Props

--- 17-forms/src/App.tsx
+++ 17-forms/src/App.tsx
@@ -1,12 +1,12 @@
 import Header from "./components/Header.tsx";
-import Login from "./components/Login.tsx";
+import Signup from "./components/Signup.tsx";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <Signup />
       </main>
     </>
   );
