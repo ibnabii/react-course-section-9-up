a6ac8a5bfb3c46e7d891ba39358a7fbfbd5c3fff
ibnabii <someone@somewhere.net>
2024-11-15 20:42:21 +0100

    Section 20 - Redux advanced (318) - action creator instead of useEffect

--- 20-redux-advanced/src/App.tsx
+++ 20-redux-advanced/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -6,52 +6,25 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification.tsx";
 
-import { RootState } from "./store";
-import { uiActions } from "./store/ui-slice.ts";
+import { DispatchType, RootState } from "./store";
+import { sendCartData } from "./store/cart-slice.ts";
 
 function App() {
   const showCart = useSelector((state: RootState) => state.ui.showCart);
   const notification = useSelector((state: RootState) => state.ui.notification);
   const cart = useSelector((state: RootState) => state.cart);
-  const dispatch = useDispatch();
+  const dispatch: DispatchType = useDispatch();
+  const isInitial = useRef<boolean>(true);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        }),
-      );
-      const response = await fetch(
-        "https://udemyreactcourse20-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) },
-      );
-      if (!response.ok) {
-        throw new Error("Failed to update cart.");
-      }
+    if (isInitial.current) {
+      isInitial.current = false;
+      console.log("initial");
+      return;
+    }
 
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        }),
-      );
-
-      return await response.json();
-    };
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: error.message,
-        }),
-      );
-    });
+    console.log("not initial");
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- 20-redux-advanced/src/store/index.ts
+++ 20-redux-advanced/src/store/index.ts
@@ -8,4 +8,4 @@ const store = configureStore({
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
-// export type DispatchType = typeof store.dispatch;
+export type DispatchType = typeof store.dispatch;
