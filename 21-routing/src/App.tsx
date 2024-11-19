5e7b3ccd3a147f9fa76d9a20ece52953b29616c9
ibnabii <someone@somewhere.net>
2024-11-19 15:48:18 +0100

    Section 21 - Router (328) alternatikve way for a router

--- 21-routing/src/App.tsx
+++ 21-routing/src/App.tsx
@@ -1,17 +1,27 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route, createRoutesFromElements } from "react-router-dom";
 import HomePage from "./pages/Home.tsx";
 import ProductsPage from "./pages/Products.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/products",
-    element: <ProductsPage />,
-  },
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductsPage />} />
+  </Route>,
+);
+
+const router = createBrowserRouter(routeDefinitions);
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <HomePage />,
+//   },
+//   {
+//     path: "/products",
+//     element: <ProductsPage />,
+//   },
+// ]);
 
 function App() {
   return <RouterProvider router={router} />;
