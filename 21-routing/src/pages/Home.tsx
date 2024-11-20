f455842046cb42ae9e0eb7349edf325e2b029b3c
ibnabii <someone@somewhere.net>
2024-11-20 10:59:15 +0100

    Section 21 - Router (333) Navigate programmatically

--- 21-routing/src/pages/Home.tsx
+++ 21-routing/src/pages/Home.tsx
@@ -1,11 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate("/products");
+  };
   return (
     <>
       <h1>My home page</h1>
       <p>
         Go to <Link to="/products">products page</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate programmatically</button>
       </p>
     </>
   );
