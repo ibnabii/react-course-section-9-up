95bec2992c557240d1a9786a14861f49becd177f
ibnabii <someone@somewhere.net>
2024-12-27 20:35:23 +0100

    Section 21 - Router advanced (378) defer()

--- 21-routing-adv/src/pages/Events.tsx
+++ 21-routing-adv/src/pages/Events.tsx
@@ -1,6 +1,7 @@
 import EventsList from "../components/EventsList";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, defer, Await } from "react-router-dom";
 import { EventType } from "../components/EventItem.tsx";
+import {Suspense} from "react";
 
 type LoaderResponseType = { events: EventType[] };
 
@@ -8,22 +9,35 @@ function EventsPage() {
   const data = useLoaderData() as LoaderResponseType;
   // console.log(data);
   // if ("isError" in data) return <p>{data.message}</p>;
-  const events: EventType[] = data.events;
+
   return (
     <>
-      <EventsList events={events} />
+      <Suspense fallback={<p style={{textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={data.events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
+      {/*<EventsList events={events} />*/}
     </>
   );
 }
 
 export default EventsPage;
 
-export async function loader(): Promise<LoaderResponseType> {
+async function loadEvents(): Promise<LoaderResponseType> {
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok)
     throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
       status: 500,
     });
 
-  return await response.json();
+  const resData =  await response.json();
+  return resData.events;
+}
+
+export function loader() {
+  return defer({
+  //   the key is arbitrary here
+    events: loadEvents(),
+  })
 }
