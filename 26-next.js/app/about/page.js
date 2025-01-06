b014241230f2864116884306140e80463fb4cc6e
ibnabii <someone@somewhere.net>
2025-01-06 17:17:23 +0100

    Section 26 - Next.js finished intro

---
+++ 26-next.js/app/about/page.js
@@ -0,0 +1,10 @@
+import Header from "@/components/header";
+
+export default function AboutPage() {
+  return (
+    <main>
+      <Header />
+      <h1>About page</h1>
+    </main>
+  );
+}
---
+++ 26-next.js/app/blog/[slug]/page.js
@@ -0,0 +1,9 @@
+export default function BlogPostPage({ params }) {
+  console.log(params);
+
+  return (
+    <main>
+      <h1>Blog Post</h1>
+    </main>
+  );
+}
