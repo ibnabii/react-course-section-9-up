9bca542d89a192a285cfab8f6f52dc93060393df
ibnabii <someone@somewhere.net>
2025-01-08 11:45:59 +0100

    Section 26 - Next.js foodie app (453) - error handling

--- 26-next.js/lib/meals.js
+++ 26-next.js/lib/meals.js
@@ -4,5 +4,6 @@ const db = sql("meals.db");
 export async function getMeals() {
   // artificial 2sec delay
   await new Promise((resolve) => setTimeout(resolve, 2000));
+  // throw new Error("Loading Meals failed");
   return db.prepare("SELECT * FROM meals").all();
 }
