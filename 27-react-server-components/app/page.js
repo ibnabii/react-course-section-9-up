e0b025d139b42b77097e6a92492d37e953ac8538
ibnabii <someone@somewhere.net>
2025-01-12 08:46:12 +0100

    Section 27 - React Server Components - Suspense after using use() Hook

--- 27-react-server-components/app/page.js
+++ 27-react-server-components/app/page.js
@@ -4,8 +4,17 @@ import DataFetchingDemo from "@/components/DataFetchingDemo";
 import ServerActionsDemo from "@/components/ServerActionsDemo";
 import UsePromiseDemo from "@/components/UsePromisesDemo";
 import { Suspense } from "react";
+import fs from "node:fs";
 
 export default async function Home() {
+  const fetchUsersPromise = new Promise((resolve) =>
+    setTimeout(async () => {
+      const data = fs.readFileSync("dummy-db.json", "utf-8");
+      const users = await JSON.parse(data);
+      resolve(users);
+    }, 3000),
+  );
+
   return (
     <main>
       <RSCDemo />
@@ -13,7 +22,7 @@ export default async function Home() {
       <DataFetchingDemo />
       <ServerActionsDemo />
       <Suspense fallback={<p>Loading...</p>}>
-        <UsePromiseDemo />
+        <UsePromiseDemo usersPromise={fetchUsersPromise} />
       </Suspense>
     </main>
   );
