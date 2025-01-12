5a7015ad1841c70a2c1bc33e563dc66304c5a376
ibnabii <someone@somewhere.net>
2025-01-12 12:39:30 +0100

    Section 27 - React Server Components - ErrorBoundary

--- 27-react-server-components/app/page.js
+++ 27-react-server-components/app/page.js
@@ -5,13 +5,15 @@ import ServerActionsDemo from "@/components/ServerActionsDemo";
 import UsePromiseDemo from "@/components/UsePromisesDemo";
 import { Suspense } from "react";
 import fs from "node:fs";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default async function Home() {
-  const fetchUsersPromise = new Promise((resolve) =>
+  const fetchUsersPromise = new Promise((resolve, reject) =>
     setTimeout(async () => {
       const data = fs.readFileSync("dummy-db.json", "utf-8");
       const users = await JSON.parse(data);
-      resolve(users);
+      // resolve(users);
+      reject(new Error("Failed to load users"));
     }, 3000),
   );
 
@@ -21,9 +23,11 @@ export default async function Home() {
       <ClientDemo />
       <DataFetchingDemo />
       <ServerActionsDemo />
-      <Suspense fallback={<p>Loading...</p>}>
-        <UsePromiseDemo usersPromise={fetchUsersPromise} />
-      </Suspense>
+      <ErrorBoundary fallback="Something went wrong...">
+        <Suspense fallback={<p>Loading...</p>}>
+          <UsePromiseDemo usersPromise={fetchUsersPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
